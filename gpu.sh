87c40bef686c404c07708140c1b6477d95c41297
therol7 <146731345+therol7@users.noreply.github.com>
2023-10-02 16:06:44 +0200

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -15,7 +15,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x8aD959f380c429EeA626034577DC62eD5DD99B58/account = 0x8aD959f380c429EeA626034577DC62eD5DD99B58/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x8aD959f380c429EeA626034577DC62eD5DD99B58/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
